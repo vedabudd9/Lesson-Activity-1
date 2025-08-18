09c860b60068fedf25ad9125b485a5ce519f76ee
vedabudd9 <vedabudd9@gmail.com>
2025-08-17 21:11:57 -0400

    Activity 8.py

--- Activity 8.py
+++ Activity 8.py
@@ -4,11 +4,11 @@ image = cv2.imread('example.jpg')
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-resized_image = cv2.resize(gray_image, (224,224))
+resized_image = cv2.resize(gray_image, (224, 224))
 
-cvs.imshow('Processed Image', resized_image)
+cv2.imshow('Processed Image', resized_image)
 
-key = cvs.waitKey(0)
+key = cv2.waitKey(0)
 
 if key == ord('s'):
     cvs.imwrite('grayscale_resized_image.jpg', resized_image)
