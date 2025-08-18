25ad0662da6d633b64d7069e9a43c531f36171dd
vedabudd9 <vedabudd9@gmail.com>
2025-08-17 21:03:44 -0400

    Activity 8.py

---
+++ Activity 8.py
@@ -0,0 +1,20 @@
+import cv2 
+
+image = cv2.imread('example.jpg')
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+resized_image = cv2.resize(gray_image, (224,224))
+
+cvs.imshow('Processed Image', resized_image)
+
+key = cvs.waitKey(0)
+
+if key == ord('s'):
+    cvs.imwrite('grayscale_resized_image.jpg', resized_image)
+else:
+        print("Image not saved")
+
+cvs.destroyAllWindows()
+
+print(f"Processed Image Dimensions: {resized_image.shape}")
